7cb65ce174453e8707e70cc900c585195b9d120b
saiyadaz <161524327+saiyadaz@users.noreply.github.com>
2024-12-04 11:35:54 -0500

    edit securiy group

--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@ resource "aws_instance" "instance" {
   count                  = length(var.components)
   ami                    = "ami-02082c2bb2f49f709"
   instance_type          = "t3.small"
-  vpc_security_group_ids = ["sg-06b8bece9ed37b3dc"]
+  vpc_security_group_ids = ["sg-0719fd3602de52422"]
   tags = {
     Name = var.components[count.index]
   }
