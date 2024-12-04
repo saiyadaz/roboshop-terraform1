a17d2fc83de70b49502bab2fae2c4788f0aa9d34
saiyadaz <161524327+saiyadaz@users.noreply.github.com>
2024-12-04 11:22:18 -0500

    add instances

--- main.tf
+++ main.tf
@@ -1,3 +1,27 @@
+variable "components" {
+  default = ["frontend", "catalogue", "cart", "user", "shipping", "payment", "dispatch"]
+}
+
+resource "aws_instance" "instance" {
+  count                  = length(var.components)
+  ami                    = "ami-0d035b0fa9820ac92"
+  instance_type          = "t3.small"
+  vpc_security_group_ids = ["sg-06b8bece9ed37b3dc"]
+  tags = {
+    Name = var.components[count.index]
+  }
+}
+
+resource "aws_route53_record" "record" {
+  count   = length(var.components)
+  name    = var.components[count.index]
+  type    = "A"
+  zone_id = "Z02385991VFZVIKN8WPK8"
+  records = [aws_instance.instance[count.index].private_ip]
+  ttl     = 3
+}
+
+
 module "vpc" {
   source = "./modules/vpc"
 
