d98e02700c4f221963d914ed3ca23da1b74a753c
saiyadaz <161524327+saiyadaz@users.noreply.github.com>
2024-12-31 23:06:32 -0500

    edit eks---main.tf

--- modules/eks/main.tf
+++ modules/eks/main.tf
@@ -1,20 +1,26 @@
 resource "aws_eks_cluster" "cluster" {
   name     = "${var.env}-eks"
   role_arn = aws_iam_role.cluster-role.arn
+
   vpc_config {
     subnet_ids = var.subnet_ids
   }
+
   encryption_config {
     provider {
       key_arn = var.kms_key_id
     }
     resources = ["secrets"]
   }
+
 }
+
 resource "aws_launch_template" "main" {
   name = "eks-${var.env}"
+
   block_device_mappings {
     device_name = "/dev/xvda"
+
     ebs {
       volume_size           = 100
       encrypted             = true
@@ -22,6 +28,7 @@ resource "aws_launch_template" "main" {
       delete_on_termination = true
     }
   }
+
   tag_specifications {
     resource_type = "instance"
     tags = {
@@ -29,29 +36,68 @@ resource "aws_launch_template" "main" {
     }
   }
 }
-resource "aws_eks_node_group" "main" {
+
+resource "aws_eks_node_group" "memory" {
+  cluster_name    = aws_eks_cluster.cluster.name
+  node_group_name = "${var.env}-eks-ng-3"
+  node_role_arn   = aws_iam_role.node-role.arn
+  subnet_ids      = var.subnet_ids
+  capacity_type   = "SPOT"
+  instance_types  = ["r7i.large","r7i.xlarge","r6i.large","r6i.xlarge"]
+
+  launch_template {
+    name    = "eks-${var.env}"
+    version = "$Latest"
+  }
+
+  scaling_config {
+    desired_size = 1
+    max_size     = 10
+    min_size     = 1
+  }
+
+  update_config {
+    max_unavailable = 1
+  }
+
+  labels = {
+    appType = "memory-intensive"
+  }
+
+}
+
+resource "aws_eks_node_group" "general" {
   cluster_name    = aws_eks_cluster.cluster.name
   node_group_name = "${var.env}-eks-ng-1"
   node_role_arn   = aws_iam_role.node-role.arn
   subnet_ids      = var.subnet_ids
   capacity_type   = "SPOT"
-  instance_types  = ["t3.large"]
+  instance_types  = ["t3.xlarge"]
+
   launch_template {
     name    = "eks-${var.env}"
     version = "$Latest"
   }
+
   scaling_config {
     desired_size = 1
-    max_size     = 2
+    max_size     = 10
     min_size     = 1
   }
+
   update_config {
     max_unavailable = 1
   }
+
+  labels = {
+    appType = "general"
+  }
+
 }
+
 resource "null_resource" "aws-auth" {
 
-  depends_on = [aws_eks_node_group.main]
+  depends_on = [aws_eks_node_group.general]
 
   provisioner "local-exec" {
     command = <<EOF
