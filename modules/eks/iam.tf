fff5fd0ee5a3536ac3022d1d6af79c03f90d6317
saiyadaz <161524327+saiyadaz@users.noreply.github.com>
2024-12-31 23:03:14 -0500

    edit extrernal dns eks iam policy

--- modules/eks/iam.tf
+++ modules/eks/iam.tf
@@ -1,5 +1,6 @@
 resource "aws_iam_role" "cluster-role" {
   name = "${var.env}-eks-role"
+
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
     Statement = [
@@ -13,20 +14,26 @@ resource "aws_iam_role" "cluster-role" {
       },
     ]
   })
+
   tags = {
     Name = "${var.env}-eks-role"
   }
 }
+
 resource "aws_iam_role_policy_attachment" "eks-AmazonEKSClusterPolicy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
   role       = aws_iam_role.cluster-role.name
 }
+
 resource "aws_iam_role_policy_attachment" "eks-AmazonEKSVPCResourceController" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSVPCResourceController"
   role       = aws_iam_role.cluster-role.name
 }
+
+
 resource "aws_iam_role" "node-role" {
   name = "${var.env}-eks-node-role"
+
   assume_role_policy = jsonencode({
     Statement = [{
       Action = "sts:AssumeRole"
@@ -38,19 +45,26 @@ resource "aws_iam_role" "node-role" {
     Version = "2012-10-17"
   })
 }
+
 resource "aws_iam_role_policy_attachment" "node-AmazonEKSWorkerNodePolicy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
   role       = aws_iam_role.node-role.name
 }
+
 resource "aws_iam_role_policy_attachment" "node-AmazonEKS_CNI_Policy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
   role       = aws_iam_role.node-role.name
 }
+
 resource "aws_iam_role_policy_attachment" "node-AmazonEC2ContainerRegistryReadOnly" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
   role       = aws_iam_role.node-role.name
 }
 
+resource "aws_iam_role_policy_attachment" "node-AmazonEBSCSIDriverPolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy"
+  role       = aws_iam_role.node-role.name
+}
 
 resource "aws_iam_policy" "node-externalDNS" {
   name        = "${var.env}-node-externalDNS"
@@ -85,11 +99,49 @@ resource "aws_iam_policy" "node-externalDNS" {
 EOF
 }
 
-resource "aws_iam_role_policy_attachment" "node-AmazonEBSCSIDriverPolicy" {
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy"
-  role       = aws_iam_role.node-role.name
-}
 resource "aws_iam_role_policy_attachment" "node-externalDNS" {
   policy_arn = aws_iam_policy.node-externalDNS.arn
+  role       = aws_iam_role.node-role.name
+}
+
+resource "aws_iam_policy" "node-clusterAutoScaler" {
+  name        = "${var.env}-node-clusterAutoScaler"
+  path        = "/"
+  description = "${var.env}-node-clusterAutoScaler"
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Action": [
+        "autoscaling:DescribeAutoScalingGroups",
+        "autoscaling:DescribeAutoScalingInstances",
+        "autoscaling:DescribeLaunchConfigurations",
+        "autoscaling:DescribeScalingActivities",
+        "autoscaling:DescribeTags",
+        "ec2:DescribeImages",
+        "ec2:DescribeInstanceTypes",
+        "ec2:DescribeLaunchTemplateVersions",
+        "ec2:GetInstanceTypesFromInstanceRequirements",
+        "eks:DescribeNodegroup"
+      ],
+      "Resource": ["*"]
+    },
+    {
+      "Effect": "Allow",
+      "Action": [
+        "autoscaling:SetDesiredCapacity",
+        "autoscaling:TerminateInstanceInAutoScalingGroup"
+      ],
+      "Resource": ["*"]
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy_attachment" "node-clusterAutoScaler" {
+  policy_arn = aws_iam_policy.node-clusterAutoScaler.arn
   role       = aws_iam_role.node-role.name
 }
--- modules/rabbitmq/main.tf
+++ modules/rabbitmq/main.tf
@@ -1,1 +1,74 @@
+resource "aws_security_group" "main" {
+  name        = "${var.component}-${var.env}-sg"
+  description = "${var.component}-${var.env}-sg"
+  vpc_id      = var.vpc_id
+
+  ingress {
+    from_port   = 5672
+    to_port     = 5672
+    protocol    = "TCP"
+    cidr_blocks = var.server_app_port_sg_cidr
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "TCP"
+    cidr_blocks = var.bastion_nodes
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "${var.component}-${var.env}-sg"
+  }
 }
+
+resource "aws_instance" "instance" {
+  ami                    = data.aws_ami.ami.image_id
+  subnet_id              = var.subnet_ids[0]
+  instance_type          = var.instance_type
+  vpc_security_group_ids = [aws_security_group.main.id]
+
+  root_block_device {
+    encrypted  = true
+    kms_key_id = var.kms_key_id
+  }
+
+  tags = {
+    Name = var.component
+    env  = var.env
+  }
+}
+
+resource "null_resource" "rabbitmq-setup" {
+  connection {
+    host     = aws_instance.instance.private_ip
+    user     = jsondecode(data.vault_generic_secret.docdb.data_json).ansible_user
+    password = jsondecode(data.vault_generic_secret.docdb.data_json).ansible_password
+    type     = "ssh"
+  }
+
+  provisioner "file" {
+    source      = "${path.module}/rabbitmq.sh"
+    destination = "/tmp/rabbitmq.sh"
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "sudo bash /tmp/rabbitmq.sh",
+    ]
+  }
+}
+
+resource "aws_route53_record" "server" {
+  name    = "${var.component}-${var.env}"
+  type    = "A"
+  zone_id = var.zone_id
+  records = [aws_instance.instance.private_ip]
+  ttl     = 30
